30a1627210bb221a3f59702f988e8d868d624b5b
lin <mr03web@foxmail.com>
2019-02-16 17:39:49 +0800

    main

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cpp-exercise
+
 C和C++的练习
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int flag = 1923;
+    std::cout << "cout " << flag << " ";
+    cout << "Hello, world!" << endl;
+    cout << "c++" << endl;
+    return 0;
+}
