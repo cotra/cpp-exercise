4498b2731d69b89023cdc5536da79169027e4f8a
lin <mr03web@foxmail.com>
2020-05-25 11:42:24 +0800

    tz

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
 int main()
 {
-    int flag = 1923;
-    std::cout << "cout " << flag << " ";
-    cout << "Hello, world!" << endl;
     cout << "c++" << endl;
+    Sleep(3000);
+    cout << "test Hello, world!" << endl;
+
     return 0;
 }
